0ffb3713c900f21fce1fc09cf54dfae0f52d9e32
Zach Bateman <zkbateman@gmail.com>
2019-03-14 00:12:03 -0500

    Small additions to EvogressionCreature

--- evogression/creatures.py
+++ evogression/creatures.py
@@ -7,8 +7,15 @@ import random
 class EvogressionCreature():
 
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self,
+                        full_parameter_example: dict,
+                        target_parameter: str) -> None:
+
+        # hunger decreases over time proportional to this creature's regression complexity
+        # successfully "eating" a sample will increase self.hunger
+        # creature dies when self.hunger == 0
+        self.hunger = 100
+
 
 
     def __lt__(self, other):
@@ -17,5 +24,11 @@ class EvogressionCreature():
     def __gt__(self, other):
         pass
 
+    def __add__(self, other):
+        pass
+
     def __repr__(self) -> str:
         return 'EvogressionCreature'
+
+    def get_regression_func(self):
+        return '...'
