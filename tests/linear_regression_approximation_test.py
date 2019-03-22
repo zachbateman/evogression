2e3a6ccc3de68ee334f6c88fe5883b6fb7de622b
Zach Bateman <zkbateman@gmail.com>
2019-03-22 11:20:07 -0500

    Update test_data.linear_data to list of dicts; Update linear tests accordingly

--- tests/linear_regression_approximation_test.py
+++ tests/linear_regression_approximation_test.py
@@ -19,15 +19,15 @@ class TestLinearRegression(unittest.TestCase):
         best_creature = None
         for cr_index, creature in enumerate(self.creatures):
             error = 0
-            for index, x in enumerate(linear_data['x']):
-                target_calc = creature.calc_target({'x': x})
-                error += abs(target_calc - linear_data['y'][index]) ** 2
+            for d in linear_data:
+                target_calc = creature.calc_target({'x': d['x']})
+                error += abs(target_calc - d['y']) ** 2
             if error < best_error:
                 best_error = error
                 best_creature = creature
                 print(f'New best creature found!  Index: {cr_index}')
         # now have "best_creature"
-        calculated_y_values = [best_creature.calc_target({'x': x}) for x in linear_data['x']]
+        calculated_y_values = [best_creature.calc_target({'x': d['x']}) for d in linear_data]
 
         print('\nBest creature found!')
         print(f'  linear regression error^2: 50.8')
@@ -37,8 +37,8 @@ class TestLinearRegression(unittest.TestCase):
         pp(best_creature.modifiers)
         print('\n'*2)
 
-        plt.scatter(linear_data['x'], linear_data['y'])
-        plt.scatter(linear_data['x'], calculated_y_values)
+        plt.scatter([d['x'] for d in linear_data], [d['y'] for d in linear_data])
+        plt.scatter([d['x'] for d in linear_data], calculated_y_values)
         plt.show()
 
 
@@ -53,9 +53,9 @@ class TestLinearRegressionLayers(unittest.TestCase):
         best_creature = None
         for cr_index, creature in enumerate(self.creatures):
             error = 0
-            for index, x in enumerate(linear_data['x']):
-                target_calc = creature.calc_target({'x': x})
-                error += abs(target_calc - linear_data['y'][index]) ** 2
+            for d in linear_data:
+                target_calc = creature.calc_target({'x': d['x']})
+                error += abs(target_calc - d['y']) ** 2
             if error < best_error:
                 best_error = error
                 best_creature = creature
@@ -72,7 +72,7 @@ class TestLinearRegressionLayers(unittest.TestCase):
         pp(best_creature.modifiers)
         print('\n'*2)
 
-        plt.scatter(linear_data['x'], linear_data['y'])
+        plt.scatter([d['x'] for d in linear_data], [d['y'] for d in linear_data])
         plt.scatter(calcuation_x_values, calculated_y_values)
         plt.show()
 
