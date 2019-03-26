1b8ed5464e6f2ff052254134528e6152e7589995
Zach Bateman <zkbateman@gmail.com>
2019-03-25 21:42:28 -0500

    Add test for creature regression func module output with standardizer

--- tests/creature_test.py
+++ tests/creature_test.py
@@ -12,6 +12,7 @@ class TestCreatureCreation(unittest.TestCase):
     @classmethod
     def setUpClass(TestFile):
         TestCreatureCreation.creature = evogression.EvogressionCreature('y', full_parameter_example={'x': None, 'y': None})
+        TestCreatureCreation.standardizer = evogression.standardize.Standardizer(linear_data)
 
     def test_asdict(self):
         pp(self.creature.get_regression_func())
@@ -19,14 +20,19 @@ class TestCreatureCreation(unittest.TestCase):
 
     def test_calc_target(self):
         calculated_values = 0
-        for x in linear_data['x']:
-            calculated_values += self.creature.calc_target({'x': x})
+        for d in linear_data:
+            calculated_values += self.creature.calc_target(d)
         self.assertTrue(type(calculated_values) == float)
 
     def test_python_module_regression_func_output(self):
         self.creature.output_python_regression_module(output_filename='test_reg_func.py')
         self.assertTrue(os.path.exists('test_reg_func.py'))
         os.remove('test_reg_func.py')
+
+    def test_python_module_regression_func_output_with_standardizer(self):
+        self.creature.output_python_regression_module(output_filename='test_reg_func_with_standardizer.py', standardizer=self.standardizer)
+        self.assertTrue(os.path.exists('test_reg_func_with_standardizer.py'))
+        os.remove('test_reg_func_with_standardizer.py')
 
 
 
