adc36f465afab6c664fe81ac7355b168e91fff84
Zach Bateman <zkbateman@gmail.com>
2019-03-26 22:06:16 -0500

    Add 3D plotting to surface_3d_regression_evolution_test

--- tests/surface_3d_regression_evolution_test.py
+++ tests/surface_3d_regression_evolution_test.py
@@ -6,12 +6,28 @@ from test_data import surface_3d_data
 from pprint import pprint as pp
 import matplotlib
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import axes3d
 
 
 class Test3DSurfaceRegression(unittest.TestCase):
 
     def test_best_creature_3d(self):
-        evolution = evogression.evolution.CreatureEvolution('z', surface_3d_data, target_num_creatures=3000, add_random_creatures_each_cycle=False)
+        evolution = evogression.evolution.CreatureEvolution('z', surface_3d_data, target_num_creatures=10000, num_cycles=15)
+
+        x = [point_dict['x'] for point_dict in surface_3d_data]
+        y = [point_dict['y'] for point_dict in surface_3d_data]
+        z = [point_dict['z'] for point_dict in surface_3d_data]
+
+        standardized_3d_data = [evolution.standardizer.convert_parameter_dict_to_standardized(d) for d in surface_3d_data]
+        z_test = [evolution.standardizer.unstandardize_value('z', evolution.best_creature.calc_target(d)) for d in standardized_3d_data]
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+
+        ax.scatter3D(x, y, z)
+        ax.scatter3D(x, y, z_test)
+
+        plt.show()
 
 
 
