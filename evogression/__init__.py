72c9b8c4b1cca2adc8b649931cd7b491a514d4d6
Zach Bateman <zkbateman@gmail.com>
2019-05-28 16:56:49 -0500

    Add initial evolution_group.py for high-level functionality

--- evogression/__init__.py
+++ evogression/__init__.py
@@ -1,3 +1,4 @@
 from . import evolution
 from .creatures import EvogressionCreature
 from . import standardize
+from . import evolution_group
---
+++ evogression/evolution_group.py
@@ -0,0 +1,33 @@
+'''
+Module containing high-level evogression functionality to fit and summarize regressions.
+'''
+import random
+import easy_multip
+from .evolution import CreatureEvolution
+
+
+
+def evolution_group(data: list, target_param: str='', target_num_creatures: int=10000, num_cycles: int=10, num_groups: int=4) -> list:
+    '''
+    Generate a list of fully initialized CreatureEvolution objects.
+    '''
+    arg_groups = [(target_param, data, target_num_creatures, num_cycles) for _ in range(num_groups)]
+    return easy_multip.map(calculate_single_evolution, arg_groups)
+
+
+def calculate_single_evolution(arg_group):
+    '''
+    Fully initialize and return a single CreatureEvolution object.
+    Module-level function for arg to easy_multip.
+    '''
+    target_param, data, num_cr, num_cy = arg_group
+    return CreatureEvolution(target_param, data, target_num_creatures=num_cr, num_cycles=num_cy, use_multip=False, initial_creature_creation_multip=False)
+
+
+def output_group_regression_funcs(group: list):
+    '''
+    Take list of CreatureEvolution objects and output their
+    best regressions to a "regression_modules" subdir.
+    '''
+    for cr_ev in group:
+        cr_ev.output_best_regression_function_as_module()
