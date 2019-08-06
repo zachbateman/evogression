15e96180d05444f3d0cd6ce420cf54e8a0415043
Zach Bateman <zkbateman@gmail.com>
2019-08-06 17:45:24 -0500

    Add parameter_pruned_evolution_group() to evolution_group.py

--- evogression/evolution_group.py
+++ evogression/evolution_group.py
@@ -43,3 +43,43 @@ def group_parameter_usage(group: list) -> Dict[str, int]:
         for param, count in cr_ev.parameter_usefulness_count.items():
             combined_parameter_usefulness[param] += count
     return combined_parameter_usefulness
+
+
+def parameter_pruned_evolution_group(data: list, target_param: str='', max_parameters: int=10, target_num_creatures: int=10000, num_cycles: int=10, num_groups: int=4) -> List[CreatureEvolutionFittest]:
+    '''
+    Generate successive groups of CreatureEvolutionFittest objects and prune least-used
+    parameters from the input data each round until only the most useful parameters remain.
+    Finally, run a full-blown evolution cycles with the remaining parameters
+    for the saved regression modules.
+    '''
+    def num_param_to_eliminate(num_extra_param: int) -> int:
+        '''Determine how many worst-performing parameters to elimintate in a given round'''
+        if num_extra_param > 50:
+            return num_extra_param // 2
+        elif num_extra_param > 10:
+            return num_extra_param // 3
+        elif num_extra_param > 3:
+            return 2
+        elif num_extra_param > 0:
+            return 1
+        else:
+            return 0
+
+    num_parameters = len(data[0].keys()) - 1
+    while num_parameters > max_parameters:
+        group = evolution_group(data, target_param, target_num_creatures // 2, num_cycles // 2, num_groups)
+
+        parameter_usage = [(param, count) for param, count in group_parameter_usage(group).items()]
+        ranked_parameters = sorted(parameter_usage, key=lambda tup: tup[1])
+        print(ranked_parameters)
+        dead_params = [t[0] for t in ranked_parameters[:num_param_to_eliminate(num_parameters - max_parameters)]]
+        for data_point in data:
+            for param in dead_params:
+                del data_point[param]
+        num_parameters = len(data[0].keys()) - 1
+
+    final_group = evolution_group(data, target_param, target_num_creatures, num_cycles, num_groups)
+    print('parameter_pruned_evolution_group complete.  Final Parameter usage counts below:')
+    for param, count in group_parameter_usage(final_group).items():
+        print(f'  {count}: {param}')
+    return final_group
