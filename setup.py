b8d5986edd1aba854bf954267ea11bf8881955d4
Zach Bateman <zkbateman@gmail.com>
2020-02-29 20:08:00 -0600

    Fix bugs in cython extension handling; update version

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ with open('README.md', 'r') as f:
     long_description = f.read()
 
 extensions = [
-    Extension('evogression.single_layer_calc', ['evogression/single_layer_calc.pyx']),
-    Extension('evogression.generate_parameter_coefficients_calc', ['evogression.generate_parameter_coefficients_calc.pyx']),
+    Extension('evogression.calc_target_cython', ['evogression/calc_target_cython.pyx']),
+    Extension('evogression.generate_parameter_coefficients_calc', ['evogression/generate_parameter_coefficients_calc.pyx']),
 ]
 
 
 setup(name='evogression',
-      version='0.2.1',
+      version='0.2.2',
       packages=find_packages(),
       license='MIT',
       author='Zach Bateman',
@@ -21,7 +21,7 @@ setup(name='evogression',
       long_description=long_description,
       long_description_content_type='text/markdown',
       url='https://github.com/zachbateman/evogression.git',
-      download_url='https://github.com/zachbateman/evogression/archive/v_0.2.1.tar.gz',
+      download_url='https://github.com/zachbateman/evogression/archive/v_0.2.2.tar.gz',
       keywords=['REGRESSION', 'MACHINE', 'LEARNING', 'EVOLUTION'],
       install_requires=['tqdm', 'easy_multip'],
       classifiers=['Development Status :: 3 - Alpha',
