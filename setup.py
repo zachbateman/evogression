014132d178b26c6d2d3810efa604034980dc30fa
Zach Bateman <zkbateman@gmail.com>
2020-02-29 20:02:52 -0600

    Switch to using setuptools instead of distutils and improve Cython extension...izing; update version

--- setup.py
+++ setup.py
@@ -1,28 +1,27 @@
-import setuptools
-from distutils.core import setup
-from distutils.extension import Extension
+from setuptools import setup, find_packages
+from setuptools.extension import Extension
 from Cython.Build import cythonize
 
 
 with open('README.md', 'r') as f:
     long_description = f.read()
 
-# extensions = [
-    # Extension('evogression.single_layer_calc', ['evogression/single_layer_calc.pyx']),
-    # Extension('evogression.generate_parameter_coefficients_calc', ['evogression.generate_parameter_coefficients_calc.pyx']),
-# ]
+extensions = [
+    Extension('evogression.single_layer_calc', ['evogression/single_layer_calc.pyx']),
+    Extension('evogression.generate_parameter_coefficients_calc', ['evogression.generate_parameter_coefficients_calc.pyx']),
+]
 
 
 setup(name='evogression',
-      version='0.2.0',
-      packages=['evogression'],
+      version='0.2.1',
+      packages=find_packages(),
       license='MIT',
       author='Zach Bateman',
       description='Evolutionary Regression',
       long_description=long_description,
       long_description_content_type='text/markdown',
       url='https://github.com/zachbateman/evogression.git',
-      download_url='https://github.com/zachbateman/evogression/archive/v_0.2.0.tar.gz',
+      download_url='https://github.com/zachbateman/evogression/archive/v_0.2.1.tar.gz',
       keywords=['REGRESSION', 'MACHINE', 'LEARNING', 'EVOLUTION'],
       install_requires=['tqdm', 'easy_multip'],
       classifiers=['Development Status :: 3 - Alpha',
@@ -32,6 +31,5 @@ setup(name='evogression',
                     'Programming Language :: Python :: 3.7',
                     'Programming Language :: Python :: 3.8',
                     ],
-    # ext_modules=cythonize(extensions)
-    ext_modules=cythonize(['evogression\\*.pyx'])
+    ext_modules=cythonize(extensions)
 )
