58ef247e33c9c9c46ed3817fc3b21228e7f9fc98
Zach Bateman <zkbateman@gmail.com>
2020-05-06 23:03:59 -0500

    Add second algorithm to mutability testing

--- tests/self_optimization/mutability_testing.py
+++ tests/self_optimization/mutability_testing.py
@@ -12,9 +12,8 @@ from functools import lru_cache
 
 
 
-def main():
-    data = surface_3d_data
-    data = [d for i, d in enumerate(data) if i % 10 == 0]
+def main1():
+    data = [d for i, d in enumerate(surface_3d_data) if i % 10 == 0]
     standardizer = evogression.standardize.Standardizer(data)
     standardized_data = standardizer.get_standardized_data()
     unst_val = standardizer.unstandardize_value
@@ -51,6 +50,45 @@ def get_evogressioncreature(iteration):
                                            mutability = 0.5 * (iteration / 50000))
 
 
+def main2():
+    data = [d for i, d in enumerate(surface_3d_data) if i % 10 == 0]
+    standardizer = evogression.standardize.Standardizer(data)
+    standardized_data = standardizer.get_standardized_data()
+    unst_val = standardizer.unstandardize_value
+
+    mutability_best_errors = {}
+    for mutability in tqdm.tqdm([0.00001, 0.0001, 0.001, 0.01, 0.05, 0.1, 0.25, 0.5, 0.75, 1.0]):
+        creatures = [evogression.EvogressionCreature('z', full_parameter_example={'x': None, 'y': None, 'z': None}, mutability=mutability)
+                                for _ in range(1500)]
+
+        for cr in creatures:
+            cr.error = 0
+
+        for cr in creatures:
+             for d in standardized_data:
+                try:
+                    cr.error += abs(unst_val('z', cr.calc_target(d)) - unst_val('z', d['z'])) ** 2
+                except OverflowError:
+                    cr.error = 10 ** 10
+
+        errors = sorted(cr.error for cr in creatures)
+        mutability_best_errors[mutability] = errors[:30]
+
+    mutabilities = []
+    errors = []
+    for mut, error_lst in mutability_best_errors.items():
+        mutabilities.extend([mut for _ in range(len(error_lst))])
+        errors.extend(error_lst)
+
+    df = pandas.DataFrame()
+    df['Mutability'] = mutabilities
+    df['Error'] = errors
+    statplot.scatterplot(df, xvar='Mutability', yvar='Error', alpha=0.3)
+    statplot.distribution_plot(df, bin_col='Mutability', result_col='Error')
+
+
+
 
 if __name__ == '__main__':
-    evolution = main()
+    main1()
+    main2()
