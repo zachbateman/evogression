6a8a66475ba73be3b998e3a4e11da5bcfa07643b
Zach Bateman <zkbateman@gmail.com>
2020-05-09 14:39:05 -0500

    Performance improvement tweak in calc_target_cython

--- evogression/calc_target_cython.pyx
+++ evogression/calc_target_cython.pyx
@@ -14,7 +14,7 @@ cpdef double calc_target_cython(dict parameters, dict modifiers, list layer_str_
 
 cdef double calc_single_layer_target_cython(dict parameters, dict modifiers, str layer_name, double previous_T):
 
-    cpdef double T = 0
+    cdef double T = 0
     cdef str param
     cdef double value
     cdef dict layer_modifiers = modifiers[layer_name]
@@ -36,10 +36,15 @@ cdef double calc_single_layer_target_cython(dict parameters, dict modifiers, str
 cdef double param_value_component(dict layer_modifiers, str param, double value):
 
     cdef dict mods
+    cdef double C, B, Z, X
 
     try:
         mods = layer_modifiers[param]
-        return mods['C'] * (mods['B'] * value + mods['Z']) ** mods['X']
+        C = mods['C']
+        B = mods['B']
+        Z = mods['Z']
+        X = mods['X']
+        return C * (B * value + Z) ** X
     except KeyError:  # if param is not in self.modifiers[layer_name]
         return 0
     except ZeroDivisionError:  # could occur if exponent is negative
