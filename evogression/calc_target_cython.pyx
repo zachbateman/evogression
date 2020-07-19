6fdcde64674fadbcb20828b693cfa76de2be4e01
Zach Bateman <zkbateman@gmail.com>
2020-07-19 12:45:09 -0500

    Improve speed of Cython modules (~20%!)

--- evogression/calc_target_cython.pyx
+++ evogression/calc_target_cython.pyx
@@ -7,7 +7,7 @@ cpdef double calc_target_cython(dict parameters, dict modifiers):
 
     cdef str layer_name
     cpdef double T = -99999  # bogus value for first layer
-    for layer_name in modifiers:
+    for layer_name in modifiers:  # DEPENDING ON ORDERED DICTIONARY HERE!!!
         T = calc_single_layer_target_cython(parameters, modifiers, layer_name, T)
     return T
 
@@ -17,15 +17,18 @@ cdef double calc_single_layer_target_cython(dict parameters, dict modifiers, str
 
     cdef double T = 0
     cdef str param
-    cdef double value
+    cdef double param_value
     cdef dict layer_modifiers = modifiers[layer_name]
+    cdef (double, double, double, int) coef
 
     cdef double C, B, Z, X
 
-    for param, value in parameters.items():
+    for param in parameters:
         try:
-            C, B, Z, X = layer_modifiers[param]  # unpacked namedtuple
-            T += C * (B * value + Z) ** X
+            coef = layer_modifiers[param]  # turn namedtuple into cython ctuple "coef"
+            C, B, Z, X = coef
+            param_value = parameters[param]  # looking up value here instead of in the "for ..." line as would do in Python is faster in Cython!!!
+            T += C * (B * param_value + Z) ** X
         except KeyError:  # if param is not in self.modifiers[layer_name]
             pass
         except OverflowError:
@@ -33,16 +36,14 @@ cdef double calc_single_layer_target_cython(dict parameters, dict modifiers, str
 
     if previous_T != -99999:
         try:
-            C, B, Z, X = layer_modifiers['T']  # unpacked namedtuple
+            coef = layer_modifiers['T']  # turn namedtuple into cython ctuple "coef"
+            C, B, Z, X = coef
             T += C * (B * previous_T + Z) ** X
         except KeyError:  # if param is not in self.modifiers[layer_name]
             pass
         except OverflowError:
             T += 10 ** 150  # really big number should make this creature die if crazy bad calculations (overflow)
 
-    try:
-        T += layer_modifiers['N']
-    except KeyError:
-        pass
+    T += layer_modifiers['N']
 
     return T
--- evogression/generate_parameter_coefficients_calc.pyx
+++ evogression/generate_parameter_coefficients_calc.pyx
@@ -4,15 +4,14 @@ Cython implementation of EvogressionCreature._generate_parameter_coefficients()
 import random
 
 
-cpdef tuple generate_parameter_coefficients_calc():
+cpdef (double, double, double, int) generate_parameter_coefficients_calc():
 
-    cpdef double C
-    cpdef double B
-    cpdef double Z
-    cpdef double X
+    cdef double C, B, Z
+    cdef int X
 
     rand_rand = random.random  # local for speed
     rand_tri = random.triangular  # local for speed
+
     C = 1 if rand_rand() < 0.4 else rand_tri(0, 2, 1)
     B = 1 if rand_rand() < 0.3 else rand_tri(0, 2, 1)
     Z = 0 if rand_rand() < 0.4 else rand_tri(-2, 2, 0)
