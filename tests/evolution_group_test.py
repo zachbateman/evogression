594f01969ed2bdd0f9fcfa525870c37db2cadc3c
Zach Bateman <zkbateman@gmail.com>
2020-09-19 01:10:06 -0500

    Update tests; update version

--- tests/evolution_group_test.py
+++ tests/evolution_group_test.py
@@ -8,7 +8,7 @@ from test_data_many_dimensions import data as many_d_data
 
 class TestGroupFunctions(unittest.TestCase):
     def test_evolution_group(self):
-        group = evogression.evolution_group(linear_data, target_param='y', group_size=6,
+        group = evogression.evolution_group('y', linear_data, group_size=6,
             num_cycles=3, optimize=10, max_layers=2, progressbar=False)
         self.assertTrue(len(group) == 6)
         layer_counts = [evo.best_creature.max_layers for evo in group]
@@ -16,7 +16,7 @@ class TestGroupFunctions(unittest.TestCase):
 
 
     def test_parameter_usage_count(self):
-        group = evogression.groups.evolution_group(many_d_data, target_param='Target', num_creatures=500, num_cycles=10, group_size=3, optimize=False)
+        group = evogression.groups.evolution_group('Target', many_d_data, num_creatures=500, num_cycles=10, group_size=3, optimize=False)
         for ev in group:
             print(ev.parameter_usefulness_count)
             self.assertTrue(len(ev.parameter_usefulness_count) > 0)
